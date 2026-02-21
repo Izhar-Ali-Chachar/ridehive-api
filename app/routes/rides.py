693f1ae0de39562e13173c4e50254f33fbe6cd34
Izhar-Ali-Chachar <izharizharali057@gmail.com>
2026-02-22 02:17:06 +0500

    added the rides endpoints

--- app/routes/rides.py
+++ app/routes/rides.py
@@ -1,10 +1,13 @@
 from fastapi import APIRouter, HTTPException
+from datetime import datetime
 from app.db.session import sessionDep
 
 from app.db.models import (
-    Rides, Riders, Drivers, Fares,
-    RidesStatus, DriverStatus
+    Rides, Riders, Drivers, Fares, Payment,
+    RidesStatus, DriverStatus, PaymentStatus
 )
+
+from sqlmodel import select
 
 router = APIRouter(prefix="rides")
 
@@ -85,11 +88,148 @@ async def accepted_ride(ride_id: int, driver_id: int, session: sessionDep):
             detail="Driver is not available"
         )
     
-    ride.status == RidesStatus.ACCEPTED
+    ride.status = RidesStatus.ACCEPTED
 
-    driver.status == DriverStatus.ON_TRIP
+    driver.status = DriverStatus.ON_TRIP
 
     session.add(ride)
     session.add(driver)
     await session.commit()
     await session.refresh(ride)
+
+    return {
+        "message": "Ride accepted successfully",
+        "ride_id": ride.id,
+        "status": ride.status,
+        "driver_status": driver.status
+    }
+
+
+@router.post("{ride_id}/started")
+async def ride_started(
+    ride_id: int,
+    session: sessionDep
+):
+    ride = await session.get(Rides, ride_id)
+    if not ride:
+        raise HTTPException(
+            status_code=400,
+            detail="Ride not found"
+        )
+    
+    if ride.status != RidesStatus.ACCEPTED:
+        raise HTTPException(
+            status_code=400,
+            detail="Ride cannot be accepted. Current status: {ride.status}"
+        )
+    
+    ride.status = RidesStatus.IN_PROGRESS
+    ride.start_time = datetime.now()
+
+    session.add(ride)
+    await session.commit()
+    await session.refresh(ride)
+
+    return {
+        "message": "Ride started",
+        "ride_id": ride.id,
+        "status": ride.status,
+        "start_time": ride.start_time
+    }
+
+@router.post("{ride_id}/completed")
+async def ride_completed(ride_id: int, session: sessionDep):
+    ride = await session.get(Rides, ride_id)
+    if not ride:
+        raise HTTPException(
+            status_code=400,
+            detail="Ride not found"
+        )
+    
+    if ride.status != RidesStatus.IN_PROGRESS:
+        raise HTTPException(
+            status_code=400,
+            detail="Ride cannot be in_progress. Current status: {ride.status}"
+        )
+    
+    driver = await session.get(Drivers, ride.driver_id)
+    if not driver:
+        raise HTTPException(
+            status_code=400,
+            detail="Driver not found"
+        )
+
+    ride.status = RidesStatus.COMPLETED
+    ride.end_time = datetime.now()
+
+    driver.status = DriverStatus.ONLINE
+
+    session.add(ride)
+    session.add(driver)
+    await session.commit()
+    await session.refresh(ride)
+
+    return {
+        "message": "Ride completed",
+        "ride_id": ride.id,
+        "status": ride.status,
+        "start_time": ride.start_time,
+        "end_time": ride.end_time
+    }
+
+@router.post("{ride_id}/payment")
+async def ride_payment(ride_id: int, session: sessionDep):
+    ride = await session.get(Rides, ride_id)
+    if not ride:
+        raise HTTPException(
+            status_code=400,
+            detail="Ride not found"
+        )
+    
+    if ride.status != RidesStatus.COMPLETED:
+        raise HTTPException(
+            status_code=400,
+            detail="Ride cannot be completed. Current status: {ride.status}"
+        )
+    
+    result = await session.execute(
+        select(Payment).where(Payment.ride_id == ride_id)
+    )
+    existing_payment = result.scalar_one_or_none()
+
+    if existing_payment:
+        raise HTTPException(
+            status_code=400,
+            detail="Payment already exists for this ride"
+        )
+    
+    fare = await session.get(Fares, ride.fares_id)
+    if not fare:
+        raise HTTPException(status_code=404, detail="Fare not found")
+    
+    rider = await session.get(Riders, ride.rider_id)
+    if not rider:
+        raise HTTPException(status_code=404, detail="Rider not found")
+
+    total_amount = fare.base_price * fare.surge_multiplier
+
+    payment = Payment(
+        rider_id = ride.rider_id,
+        ride_id = ride_id,
+        amount = total_amount,
+        payment_method = rider.payment_method,
+        staus = PaymentStatus.COMPLETED
+    )
+
+    session.add(payment)
+    await session.commit()
+    await session.refresh(payment)
+
+    return {
+        "message": "Payment successful",
+        "payment_id": payment.id,
+        "ride_id": ride_id,
+        "amount": payment.amount,
+        "payment_method": payment.payment_method,
+        "status": payment.status
+    }
