200117e18ce1527a01e1db17579d0b3f582090f6
Izhar-Ali-Chachar <izharizharali057@gmail.com>
2026-02-22 01:00:09 +0500

    basic setup of main file

---
+++ app/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+
+from contextlib import asynccontextmanager
+
+from app.db.session import create_tables
+
+@asynccontextmanager
+async def spand_handler(app: FastAPI):
+    await create_tables()
+    yield
+
+app = FastAPI(lifespan=spand_handler)
+
+@app.get("/")
+def health_check():
+    return {"status": "ok"}
