e86a9d5616b054cf0d23c11b307c442f0172a2d2
Izhar-Ali-Chachar <izharizharali057@gmail.com>
2026-02-23 01:54:21 +0500

    added the register endpoint of rider and also the event triggers as well

--- services/rider_service/events.py
+++ services/rider_service/events.py
@@ -1,1 +1,30 @@
 import redis
+import json
+
+from datetime import datetime
+
+r = redis.Redis(
+    host = "local",
+    port = 6379,
+    decode_responses = True
+)
+
+def publish_event(event_name: str, data: dict):
+    data["timestep"] = datetime.now()
+
+    payload = json.dumps(data)
+
+    r.publish(
+        event_name,
+        payload
+    )
+    print(f"Event fired: {event_name} → {data}")
+
+def event_rider_registered(rider_id: int, payment_method: str):
+    publish_event(
+        "rider.registered",
+        {
+            rider_id: rider_id,
+            payment_method: payment_method
+        }
+    )
--- services/rider_service/routes.py
+++ services/rider_service/routes.py
@@ -1,7 +1,32 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import APIRouter, HTTPException
 
+from services.rider_service.models import RiderCreate, RiderResponse
+from services.rider_service.events import event_rider_registered
 
-app = FastAPI(root_path="rider")
+from database.session import sessionDep
+from database.models import Riders
+
+app = APIRouter(prefix="/rider")
 
 @app.post("/register")
-async def register_rider(rider: )
+async def register_rider(rider: RiderCreate, session: sessionDep):
+    db_rider = Riders(
+        **rider.model_dump()
+    )
+
+    session.add(db_rider)
+    await session.commit()
+    await session.refresh(db_rider)
+
+    if not db_rider.id:
+        raise HTTPException(
+            status_code=400,
+            detail="rider id not found"
+        )
+
+    event_rider_registered(
+        db_rider.id,
+        db_rider.payment_method
+    )
+
+    return db_rider
